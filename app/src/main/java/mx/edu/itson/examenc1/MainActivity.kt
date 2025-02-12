32ec9d6069239a85af30ed1a729259aea1771d68
oloo <ooo>
2025-02-11 18:46:34 -0700

    Commit 2

--- app/src/main/java/mx/edu/itson/examenc1/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/examenc1/MainActivity.kt
@@ -11,24 +11,27 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val tipoPoliza:EditText=findViewById(R.id.etTipoPoliza)
-        val costo:EditText=findViewById(R.id.etCosto)
-        val anios:EditText=findViewById(R.id.etAnios)
-        val total:TextView=findViewById(R.id.tvTotalPoliza)
-
-        tipoPoliza.text.toString()
-
-
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var polizaSedan:Float=500F
+        var polizaDeportivos:Float=1200F
+        var polizaCamioneta:Float=700F
+
+        val tipoPoliza:EditText=findViewById(R.id.etTipoPoliza)
+        val costo:TextView=findViewById(R.id.tvCosto)
+        val anios:EditText=findViewById(R.id.etAnios)
+        val total:TextView=findViewById(R.id.tvTotal)
+
+        try {
+            var polizaTipo = tipoPoliza.text.toString()
+            var aniosObtener = anios.text.toString().toInt()
+
+        }catch (e:java.lang.Exception){
+            print("Error en la compilación")
         }
     }
 }
-fun calcularTotalPoliza(){
+fun calcularTotalPoliza(aniosPoliza:Int,polizaCosto:Float){
 
 }
